cbe1de7ea19e669901a9b513019d081eaf6287b0
hoonison <hoonisone@gmail.com>
2023-10-12 13:02:10 +0900

    path generate function

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="confspace",
-    version="0.0.1",
+    version="0.0.2",
     author="Hoonisone",
     author_email="hoonisone@gmail.com",
     description="confspace object for tuning parameter",
